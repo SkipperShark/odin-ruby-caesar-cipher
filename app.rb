e9b7590fe01ca418f43a892bfe2ff48758f9f543
sharman <sharmanfaridz@gmail.com>
2024-04-04 20:36:45 +0800

    another cleanup, clean = good

--- app.rb
+++ app.rb
@@ -7,10 +7,15 @@ shift_offset = 5
 
 def caesar_cipher( unencrypted_string, shift_offset)
 
-  a_to_z_offset = "z".ord - "a".ord
+  z_ord = "z".ord
+  Z_ord = "Z".ord
+  a_ord = "a".ord
+
+  a_to_z_offset = z_ord - a_ord + 1 # +1 as it takes 1 step to move from a to z
 
   encrypted_string_array = unencrypted_string.chars.map do |char|
 
+    char_ord = char.ord
     valid_char_to_encrypt = char.match?(/[a-zA-z]/)
 
     next char if not valid_char_to_encrypt
@@ -21,14 +26,14 @@ def caesar_cipher( unencrypted_string, shift_offset)
     char_is_lowercase = char == char.downcase
 
     a_z_swap_needed = if char_is_uppercase
-                        (char.ord + shift_offset) > "Z".ord
+                        (char_ord + shift_offset) > Z_ord
                       elsif char_is_lowercase
-                        (char.ord + shift_offset) > "z".ord
+                        (char_ord + shift_offset) > z_ord
                       else
                         false
                       end
 
-    new_ord = if a_z_swap_needed then char.ord + shift_offset - (a_to_z_offset + 1) else char.ord + shift_offset end
+    new_ord = if a_z_swap_needed then char_ord + shift_offset - (a_to_z_offset) else char_ord + shift_offset end
 
     new_char.setbyte(0, new_ord)
 
